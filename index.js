6f75b60e820df8d7587fec3d82e0c5222790897a
maurice diong <mdiong00@gmail.com>
2022-10-15 04:18:35 -0400

    commit tree d464d6ba6ef172bff379595c1fd8a9959ad1b49c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+//const newArray = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//console.log(newArray)
+const findMatching = function (array,  name) {
+       return array.filter(x => x.toUpperCase() === name.toUpperCase())
+        
+}
+
+const fuzzyMatch = function(array, name) {
+    return array.filter( x => x.startsWith(name))
+    }
+
+    const matchName = function(array,driver) {
+        return array.filter (x => x.name === driver)
+    }
+        /*caseSensitive(name));
+        }
+
+   const caseSensitive = function (x) {
+    return x;
+   }
+/*const notCaseSensitive = function (newArray, newName) { 
+    for (const nom of newArray) {
+            if (newName.toUpperCase() === nom.toUpperCase()){
+            return nom;
+    }
+}
+}*/
